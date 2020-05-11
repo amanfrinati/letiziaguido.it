15c34a5c03d889e0b3b7da78794696805dc19c1b
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2020-05-11 23:38:51 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugin: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "src", to: "dist" }],
+    }),
+  ],
+};
