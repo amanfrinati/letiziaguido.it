66c1775e28b00f23db08a2bc945ca2b0cfa47c99
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2020-06-13 00:37:01 +0200

    add content hash and optimization to webpack build

--- webpack.common.js
+++ webpack.common.js
@@ -38,5 +38,5 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-  },
+  }
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,20 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
+  output: {
+    filename: 'bundle.[contenthash].js',
+  },
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    }
+  }
 });
